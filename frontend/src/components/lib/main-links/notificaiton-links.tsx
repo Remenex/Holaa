d764b093b2e9b495c1bd3d73dd09ecb521b8cb72
leke <jaleksa388@gmail.com>
2026-01-07 13:18:34 +0100

    currently no notifications paragraph

--- frontend/src/components/lib/main-links/notificaiton-links.tsx
+++ frontend/src/components/lib/main-links/notificaiton-links.tsx
@@ -22,6 +22,9 @@ export default function NotificationLink({ icon, invites }: Props) {
     });
   };
 
+  const pendingInvites =
+    invites?.filter((invite) => invite.status === InviteStatus.PENDING) || [];
+
   return (
     <div className="relative w-[70px] h-[70px] flex items-center justify-center group">
       <Image
@@ -33,65 +36,72 @@ export default function NotificationLink({ icon, invites }: Props) {
       />
       <Icon icon={icon} iconSize={35} variation="text-white" />
 
-      {invites && invites.length > 0 && (
+      {
         <div
           className="w-[70px] h-full absolute top-0 right-0 rounded-[40px] border-0 flex flex-col items-center scrollbar-hide
           overflow-hidden transition-all duration-300 ease-in-out group-hover:overflow-y-scroll group-hover:p-4 group-hover:w-[550px] group-hover:border-[#ff581c] group-hover:border-4 group-hover:h-[300px] group-hover:notification-gradient"
         >
-          {invites.map((invite) => {
-            if (invite.status === InviteStatus.PENDING) {
-              return (
-                <div
-                  key={invite._id}
-                  className="ml-5 opacity-0 transition-opacity duration-300 ease-in-out flex flex-col justify-start group-hover:opacity-100"
-                >
-                  {/* <h2 className="text-3xl font-display ">
+          {pendingInvites.length === 0 ? (
+            <p className="opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 text-2xl font-display">
+              Trenutno nemate obavestenja
+            </p>
+          ) : (
+            pendingInvites.map((invite) => {
+              if (invite.status === InviteStatus.PENDING) {
+                return (
+                  <div
+                    key={invite._id}
+                    className="ml-5 opacity-0 transition-opacity duration-300 ease-in-out flex flex-col justify-start group-hover:opacity-100"
+                  >
+                    {/* <h2 className="text-3xl font-display ">
                   ZAHTEV ZA GLEDANJE FILMA
                 </h2> */}
-                  <div className="flex items-start mt-4 gap-3">
-                    <UserAvatar
-                      firstname={invite.fromUserId.firstName}
-                      lastname={invite.fromUserId.lastName}
-                      sizeRem={3}
-                    />
-                    <div>
-                      <p className="text-2xl leading-6">
-                        <b>
-                          {invite.fromUserId.firstName}{" "}
-                          {invite.fromUserId.lastName}
-                        </b>{" "}
-                        vas poziva da zajedno gledate “<b>film</b>”
-                      </p>
+                    <div className="flex items-start mt-4 gap-3">
+                      <UserAvatar
+                        firstname={invite.fromUserId.firstName}
+                        lastname={invite.fromUserId.lastName}
+                        sizeRem={3}
+                      />
+                      <div>
+                        <p className="text-2xl leading-6">
+                          <b>
+                            {invite.fromUserId.firstName}{" "}
+                            {invite.fromUserId.lastName}
+                          </b>{" "}
+                          vas poziva da zajedno gledate “<b>film</b>”
+                        </p>
 
-                      <div className="mt-4 flex gap-3">
-                        <div
-                          className="px-4 py-1 bg-[#52b561] flex items-center rounded-[20px] cursor-pointer"
-                          onClick={() =>
-                            handleInviteClick(invite, InviteStatus.ACCEPTED)
-                          }
-                        >
-                          <Icon icon="check" />
-                          <p>Prihvati</p>
-                        </div>
+                        <div className="mt-4 flex gap-3">
+                          <div
+                            className="px-4 py-1 bg-[#52b561] flex items-center rounded-[20px] cursor-pointer"
+                            onClick={() =>
+                              handleInviteClick(invite, InviteStatus.ACCEPTED)
+                            }
+                          >
+                            <Icon icon="check" />
+                            <p>Prihvati</p>
+                          </div>
 
-                        <div
-                          className="px-4 py-1 bg-[#c01e1e] flex items-center rounded-[20px] cursor-pointer"
-                          onClick={() =>
-                            handleInviteClick(invite, InviteStatus.DECLINED)
-                          }
-                        >
-                          <Icon icon="close" />
-                          <p>Odbij</p>
+                          <div
+                            className="px-4 py-1 bg-[#c01e1e] flex items-center rounded-[20px] cursor-pointer"
+                            onClick={() =>
+                              handleInviteClick(invite, InviteStatus.DECLINED)
+                            }
+                          >
+                            <Icon icon="close" />
+                            <p>Odbij</p>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              );
-            }
-          })}
+                );
+              }
+            })
+          )}
+          {}
         </div>
-      )}
+      }
     </div>
   );
 }
