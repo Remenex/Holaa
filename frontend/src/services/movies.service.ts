20685df3aaeb47d4534e709bf59be64fc357ee15
karadjordje <djordjeivanovic05@hotmail.com>
2026-01-13 12:09:16 +0100

    *Movie Categories Page Conected With Backend

--- frontend/src/services/movies.service.ts
+++ frontend/src/services/movies.service.ts
@@ -3,3 +3,7 @@ import { http } from "./http";
 export async function getMovie(id: string) {
   return http<Movie>(`/movies/${id}`);
 }
+
+export async function getCategories() {
+  return http<Category[]>("/categories");
+}
