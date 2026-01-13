6cd2b5ef59c5bfc5b55ecfa67786d754d335554c
karadjordje <djordjeivanovic05@hotmail.com>
2026-01-13 13:12:14 +0100

    *Index Page Movies Connected

--- backend/src/movies/controllers/movie.controller.ts
+++ backend/src/movies/controllers/movie.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   Patch,
   Post,
+  Query,
   UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
@@ -19,6 +20,21 @@ import { MovieService } from '../services/movie.service';
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
 
+  @Get('latest/:limit')
+  getLatestMovies(@Param('limit') limit: number) {
+    return this.movieService.getLatestMovies(limit);
+  }
+
+  @Get('top-rated')
+  async getTopRatedMovies(@Query('limit') limit?: string) {
+    const lim = limit ? parseInt(limit) : 10;
+    return this.movieService.getTopRatedMovies(lim);
+  }
+
+  @Get('by-category/:id')
+  async getMoviesByCategory(@Param('id') id: string) {
+    return this.movieService.getMoviesByCategory(id);
+  }
   @Get(':id')
   getMovie(@Param('id') id: string) {
     return this.movieService.getMovieById(id);
@@ -96,20 +112,5 @@ export class MovieController {
     @UploadedFiles() files: any,
   ) {
     return this.movieService.updateMovie(id, body, files);
-  }
-
-  @Get('latest')
-  async getLatestMovies() {
-    return this.movieService.getLatestMovies();
-  }
-
-  @Get('top-rated')
-  async getTopRatedMovies() {
-    return this.movieService.getTopRatedMovies();
-  }
-
-  @Get('by-category/:id')
-  async getMoviesByCategory(@Param('id') id: string) {
-    return this.movieService.getMoviesByCategory(id);
   }
 }
--- frontend/src/services/movies.service.ts
+++ frontend/src/services/movies.service.ts
@@ -15,3 +15,11 @@ export async function getCategories() {
 export async function getMoviesByCategory(id: string) {
   return http<Movie[]>(`/movies/by-category/${id}`);
 }
+
+export async function getLatestMovies() {
+  return http<Movie[]>("/movies/latest/10");
+}
+
+export async function getTopRatedMovies() {
+  return http<Movie[]>(`/movies/top-rated?limit=10`);
+}
