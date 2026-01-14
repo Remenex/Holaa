2b3f01d9bd396699acf1a6c954b463060c3f208e
leke <jaleksa388@gmail.com>
2026-01-14 20:35:12 +0100

    added recommendations

--- frontend/src/components/li/index-header/index.tsx
+++ frontend/src/components/li/index-header/index.tsx
@@ -1,12 +1,15 @@
 import RedirectButton from "@/components/lib/button/redirect-button";
 import Logo from "@/components/lib/logo";
 import UserDropdown from "@/components/user-dropdown";
+import Link from "next/link";
 
 export function IndexHeader() {
   return (
     <div className="w-full main-container flex justify-center absolute top-0 z-10">
       <div className="w-full max-w-[1720px] mt-6 flex justify-between items-center">
-        <Logo />
+        <Link href="/">
+          <Logo />
+        </Link>
         <div className="flex items-center gap-6">
           <RedirectButton
             text="POPULARNI FILMOVI"
